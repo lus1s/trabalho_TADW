a23bd04207bbb91fd1a96c40bfc58f4556c80e7c
Luís Carlos dos Santos <139286880+lus1s@users.noreply.github.com>
2024-07-03 20:32:01 -0300

    form_aluguel relacionado a pessoa física com PHP finalizado

--- sistema/dados_cliente.php
+++ sistema/dados_cliente.php
@@ -1,47 +1,75 @@
 <?php
-    session_start();
-    require_once 'conexao.php';
+session_start();
+require_once 'conexao.php';
 
-    if ($_GET['origem'] == 1) {
+if ($_GET['origem'] == 1) {
 
-        $nome_cliente = $_GET['nome_cliente'];
-        $tipo_cliente = $_GET['tipo'];
+    $nome_cliente = $_GET['nome_cliente'];
+    $tipo_cliente = $_GET['tipo'];
 
-        $_SESSION['dados'] = array($nome_cliente, $tipo_cliente);
+    $_SESSION['dados'] = array($nome_cliente, $tipo_cliente);
 
-        if ($tipo == "cpf") {
-            header('Location: pessoa_fisica.html');
-            exit();
-        } else {
-            header('Location: pessoa_juridica.html');
-            exit();
-        }
-    } elseif ($_GET['origem'] == 2) {
 
-        $cpf_cliente = $_GET['cpf'];
-        $cnh_cliente = $_GET['cnh'];
-        $endereco_cliente = $_GET['endereco'];
+    // Direciona p/ o proximo formulário a ser preenchido
 
-        $_SESSION['dados'][] = $cpf_cliente;
-        $_SESSION['dados'][] = $cnh_cliente;
-        $_SESSION['dados'][] = $endereco_cliente;
-        
-        list($nome, $tipo, $CPF, $endereco) = $_SESSION['dados'];
-
-        $sql = "INSERT INTO tb_cliente ('nome', ";
-
-        // INSERT INTO `tb_pessoa` (`cpf`, `cnh`, `tb_cliente_id_cliente`)
-// VALUES ('65421387945', '321546879564', '1');
-    } elseif ($_GET['origem'] == 3) {
-
-        $cnpj_cliente = $_GET['cpf'];
-        $endereco_cliente = $_GET['endereco'];
-
-        $_SESSION['dados'][] = $cpf;
-        $_SESSION['dados'][] = $endereco;
-
-        list($nome, $tipo, $CNPJ, $endereco) = $_SESSION['dados'];
-
-        
+    if ($tipo_cliente == "p") {
+        header('Location: pessoa_fisica.html');
+        exit();
+    } else {
+        header('Location: pessoa_juridica.html');
+        exit();
     }
-?>
+} elseif ($_GET['origem'] == 2) {
+
+    $cpf = $_GET['cpf'];
+    $cnh = $_GET['cnh'];
+    $endereco = $_GET['endereco'];
+
+    list($nome, $tipo) = $_SESSION['dados'];
+
+    $sql = "INSERT INTO `tb_cliente` (`nome_cliente`, `tipo_cliente`) VALUES ('$nome', '$tipo')";
+
+    mysqli_query($conexao, $sql);
+
+    $sql_busca = "SELECT id_cliente FROM tb_cliente";
+
+    $resultado = mysqli_query($conexao, $sql_busca);
+
+    if (mysqli_num_rows($resultado) > 0) {
+        while ($linha = (mysqli_fetch_array($resultado))) {
+            $id_cliente = $linha['id_cliente'];
+        }
+    }
+
+    $sql2 = "INSERT INTO `tb_pessoa` (`cpf`, `cnh`, `tb_cliente_id_cliente`) VALUES ('$cpf', '$cnh', $id_cliente)";
+
+    mysqli_query($conexao, $sql2);
+
+    $sql3 = "INSERT INTO `tb_enderecos` (`endereco`, `tb_cliente_id_cliente`) VALUES
+('$endereco', '$id_cliente')";
+
+
+    if (isset($_SESSION['dados'])) {
+        unset($_SESSION['dados']);
+    }
+
+    if (mysqli_query($conexao, $sql3)) {
+        header('Location: index.html');
+        exit();
+    } else {
+        header('Location: form_aluguel');
+        exit();
+    }
+
+
+
+} elseif ($_GET['origem'] == 3) {
+
+    $cnpj_cliente = $_GET['cpf'];
+    $endereco_cliente = $_GET['endereco'];
+
+    $_SESSION['dados'][] = $cpf;
+    $_SESSION['dados'][] = $endereco;
+
+    list($nome, $tipo, $CNPJ, $endereco) = $_SESSION['dados'];
+}
