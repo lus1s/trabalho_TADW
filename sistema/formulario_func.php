c2c55f7e931c7af8d5cc68a6df97f529cb7ded7b
Maria7Beatriz <mariabeatriz678@icloud.com>
2024-08-08 15:41:11 -0300

    form_funci organizado

--- sistema/formulario_func.php
+++ sistema/formulario_func.php
@@ -2,7 +2,7 @@
     require_once 'testeLogin.php';
     require_once "conexao.php";
 
-    $nome = $_GET['nome'];
+    $name = $_GET['name'];
     $cpf = $_GET['cpf'];
     $password = $_GET['password'];
 
@@ -12,9 +12,11 @@
 
     $stmt = mysqli_prepare($conexao, $sql);
 
-        mysqli_stmt_bind_param($stmt, "", $nome, $cpf, $password);
+        mysqli_stmt_bind_param($stmt, "sss", $name, $cpf, $password);
 
-    mysqli_stmt_bind_execute($stmt);
+    mysqli_stmt_execute($stmt);
+       
+    
     mysqli_stmt_close($stmt);
    
     header('Location: formulario_funci.html');
