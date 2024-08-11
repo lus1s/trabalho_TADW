c9612eecbe1d0e7085dabc95511de457c01c4d63
Luís Carlos dos Santos <139286880+lus1s@users.noreply.github.com>
2024-08-11 16:17:35 -0300

    uma leve brincadeira levada pelo meu tedio

---
+++ sistema/js/script.js
@@ -0,0 +1,9 @@
+function colors() {
+    let rgb1 = Math.floor(Math.random() * 256);
+    let rgb2 = Math.floor(Math.random() * 256);
+    let rgb3 = Math.floor(Math.random() * 256);
+
+    document.getElementById('corpo').style = "background-color: rgb(" + rgb1 + ', ' + rgb2 + ', ' + rgb3 + ");"; 
+    
+    return false;
+}
