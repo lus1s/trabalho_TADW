644c6513f729e35cb8bf122c1d5f11350a00c913
Janulha <anajuliampm2007@gmail.com>
2024-08-29 15:26:50 -0300

    teste4

--- sistema/form_veiculo.php
+++ sistema/form_veiculo.php
@@ -3,31 +3,30 @@
     require_once "conexao.php";
     require_once "operacoes.php";
 
-    $nomeveiculo = $_GET['nomeveiculo'];
-    $anoveiculo = $_GET['anoveiculo'];
-    $marcaveiculo = $_GET['marcaveiculo'];
-    $tipoveiculo = $_GET['veiculo'];
-    $corveiculo = $_GET['corveiculo'];
-    $placaveiculo = $_GET['placaveiculo'];
-    $disponibilidade = $_GET['disponibilidade'];
-    $motor = $_GET['motor'];
-    $kmrodado = $_GET['kmrodado'];
-    $descricao = $_GET['descricao'];
-    $portas = $_GET['portas'];
-    $arcondicionado = $_GET['ar'];
-    $portamala = $_GET['portamala'];
-    $tamanho = $_GET['tamanho'];
-    $cambio = $_GET['cambio'];
-    $passageiro = $_GET['passageiro'];
+    $nomeveiculo = $_GET['nomeveiculo']; #s
+    $anoveiculo = $_GET['anoveiculo'];#s
+    $marcaveiculo = $_GET['marcaveiculo'];#s
+    $tipoveiculo = $_GET['veiculo'];#i
+    $corveiculo = $_GET['corveiculo'];#s
+    $placaveiculo = $_GET['placaveiculo'];#s
+    $disponibilidade = $_GET['disponibilidade'];#i
+    $motor = $_GET['motor'];#s
+    $kmrodado = $_GET['kmrodado'];#s
+    $descricao = $_GET['descricao'];#s
+    $portas = $_GET['portas'];#i
+    $arcondicionado = $_GET['ar'];#i
+    $portamala = $_GET['portamala'];#i
+    $tamanho = $_GET['tamanho'];#i
+    $cambio = $_GET['cambio'];#i
+    $passageiro = $_GET['passageiro'];#i
     
 
 
     $sql = "INSERT INTO tb_veiculo (nome_veiculo, ano_veiculo, marca_veiculo, tipo_veiculo, cor_veiculo, placa_veiculo, estado_veiculo, motor_veiculo, 
     km_rodados, descricao_veiculo, qtd_portas, arcondicionado_veiculo, portamala_veiculo, tamanho_veiculo, cambio_veiculo, npassageiro_veiculo,)VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    
+    #       1  2  3  4  5  6  7  8  9  0  1  2  3  4  5  6
     $stmt =  mysqli_prepare($conexao, $sql);
-
-    mysqli_stmt_bind_param($stmt,"ssississssiiiiii",$nomeveiculo,$anoveiculo,$marcaveiculo, $tipoveiculo ,$corveiculo, $placaveiculo, $disponibilidade, $motor, $kmrodado, $descricao ,$portas, $arcondicionado,$portamala,$tamanho,$cambio, $passageiro);
+    mysqli_stmt_bind_param($stmt,"sssississsiiiiii", $nomeveiculo, $anoveiculo,$marcaveiculo, $tipoveiculo, $corveiculo, $placaveiculo, $disponibilidade, $motor, $kmrodado, $descricao, $portas, $arcondicionado, $portamala,$tamanho, $cambio, $passageiro);
     
 
     mysqli_stmt_execute($stmt);
