8fd07faa3bbafd031ae396b515ae8ffb0fc7cc27
Julian Victor Lopes OLiveira <julianvictorlopesoliveira@gmail.com>
2024-10-28 08:51:35 -0300

    Documentação - Julian
    Foi iniciada a documentação do SITE:
     -  página de "cadastroCliente.php"

--- sistema/cadastroCliente.php
+++ sistema/cadastroCliente.php
@@ -1,7 +1,32 @@
 <?php
+
+/**
+ * Cadastro de cliente
+ * 
+ * Esta página processa os dados e envia para o banco
+ * 
+ * @author Luís Carlos <email@email.com>
+ * 
+ * @requires conexao.php
+ * @requires testeLogin.php
+ * @requires operacoes.php
+ */
+
 require_once 'conexao.php';
 require_once 'testeLogin.php';
 require_once 'operacoes.php';
+
+/**
+ * IF - verificação
+ * 
+ * Verifica se está vindo do formulário 2 e envia para a origem
+ * 
+ * @param string    $nome           contém o nome do cliente que vem do formulário
+ * @param int       $tipo           simboliza se o cliente é pessoa física (CPF) ou pessoa jurídica (CNPJ)           
+ * @param mysqli    $conexao        conecta com o banco
+ * @param mysqli    $id_cliente     insere o cliente verificado
+ * 
+ */
 
 if ($_GET['origem'] == 2) {
         
