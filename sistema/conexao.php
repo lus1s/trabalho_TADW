ca91ab3aa1bfd23b6f3f6dbfb77e1c7410391c99
Maria7Beatriz <mariabeatriz678@icloud.com>
2024-11-08 08:18:31 -0300

    documantação carrinho dev. e conaxao

--- sistema/conexao.php
+++ sistema/conexao.php
@@ -1,8 +1,27 @@
 <?php
-    $servidor = "db";
+
+    /**
+     * conexão com o banco
+     * 
+     * Essas variáveis guardam as informações de conexão com o banco de dados;
+     *
+     * 
+     * @author Luis carlos <email@email.com>
+     * 
+     * @$servidor / db                colocou endereço do servidor do banco de dados  "db";
+     * @$usuario /                    root  nome de usuário para acessar o banco  "root";
+     * @senha / 123                   senha para autenticação  "123";
+     * @$banco / bd_veiculosFaria     nome do banco de dados que será acessado "bd_veiculosFaria";
+     *  
+     *
+     **/
+
+    $servidor = "db";       
     $usuario = "root";
     $senha = "123";
     $banco = "bd_veiculosFaria";
 
-    $conexao = mysqli_connect($servidor , $usuario , $senha , $banco);
+    //*  Essa linha cria uma conexão com o banco de dados usando as informações fornecida,
+     //* caso der certo $conexao vai conter o link de conexão para mexer com o banco. Caso contrário, indicará erro de conexão.*// 
+    $conexao = mysqli_connect($servidor , $usuario , $senha , $banco); 
 ?>
