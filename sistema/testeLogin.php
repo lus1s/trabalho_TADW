270eedca3a79cb127166c63351eab98f577c7542
Maria7Beatriz <mariabeatriz678@icloud.com>
2024-11-24 00:12:32 -0300

    Documentação teste

--- sistema/testeLogin.php
+++ sistema/testeLogin.php
@@ -1,8 +1,8 @@
 <?php
-    session_start();
+    session_start();  //Inicia a sessão no PHP, permitindo o acesso às variáveis de sessão.
     
-    if (isset($_SESSION['logado']) == false) {
-        header('Location: index.html');
-        exit();
+    if (isset($_SESSION['logado']) == false) {  //Verifica se a variável de sessão logado não está definida (ou seja, se o usuário não está logado).
+        header('Location: index.html');         //Se o usuário não estiver logado, redireciona o navegador para a página index.html.
+        exit();     //Se o usuário não estiver logado, redireciona o navegador para a página index.html.
     }
 ?>
