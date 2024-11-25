d0d4a3f9304a8bd9824f604de37aa3c7095b0772
Luís Carlos dos Santos <139286880+lus1s@users.noreply.github.com>
2024-11-25 08:01:18 -0300

    adição do FPDF

--- sistema/dados_individuais.php
+++ sistema/dados_individuais.php
@@ -1,63 +1,68 @@
 <?php
-    require_once 'testeLogin.php';
-    require_once 'operacoes.php';
-    require_once 'conexao.php';
+require_once 'testeLogin.php';
+require_once 'operacoes.php';
+require_once 'conexao.php';
 
-    $nome_cliente = $_GET['nome_cliente'];
+$nome_cliente = $_GET['nome_cliente'];
 
-    $id_cliente = $_GET['id_cliente'];
- ?> 
+$id_cliente = $_GET['id_cliente'];
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Dados: <?php echo $nome_cliente; ?></title>
 </head>
+
 <body>
     <a href="./busca_clientes.html">voltar</a> <br><br>
     <?php
 
-        $id_aluguel[] = idAluguelPorTbCliente($conexao, $id_cliente);
+    $id_aluguel[] = idAluguelPorTbCliente($conexao, $id_cliente);
 
-        $dados_cliente = dadosCompletosCliente($conexao, $id_cliente);
-        
-        if (!empty($dados_cliente)) {
-            
-            foreach ($dados_cliente as $cliente) {
-                echo "Nome do usuario: " .  $cliente['cliente'] . "<br>";
-                echo "CPF: " .  $cliente['cpf'] . " <br>";
-                echo "CNH: " .  $cliente['cnh'] . " <br>";
-                echo "Endereco: " .  $cliente['endereco'] . " <br>";
-                echo "<br><br>";
-            }
-        }else {
-            $dados_cliente = dadosCompletosEmpresa($conexao, $id_cliente);
+    $dados_cliente = dadosCompletosCliente($conexao, $id_cliente);
 
-            foreach ($dados_cliente as $cliente) {
-                echo "Nome do usuario: " .  $cliente['cliente'] . "<br>";
-                echo "CNPJ: " .  $cliente['cnpj'] . " <br>";
-                echo "FUNCIONARIO RESPONSÁVEL: " .  $cliente['funcResponsavel'] . " <br>";
-                echo "Endereco: " .  $cliente['endereco'] . " <br>";
-                echo "<br><br>";
-            }
+    if (!empty($dados_cliente)) {
+
+        foreach ($dados_cliente as $cliente) {
+            echo "Nome do usuario: " .  $cliente['cliente'] . "<br>";
+            echo "CPF: " .  $cliente['cpf'] . " <br>";
+            echo "CNH: " .  $cliente['cnh'] . " <br>";
+            echo "Endereco: " .  $cliente['endereco'] . " <br>";
+            echo "<br><br>";
         }
+    } else {
+        $dados_cliente = dadosCompletosEmpresa($conexao, $id_cliente);
 
-        echo "<table border='1'>
-                <tr>
-                <td colspan='4'>
-                    <center>
-                        Veiculos p/ devolução
-                    </center>
-                </td>
-                </tr>
-                <tr>
-                <td>Veiculo</td>
-                <td>Data</td>
-                <td>Ação</td>
-                </tr>
-            <tr>";
+        foreach ($dados_cliente as $cliente) {
+            echo "Nome do usuario: " .  $cliente['cliente'] . "<br>";
+            echo "CNPJ: " .  $cliente['cnpj'] . " <br>";
+            echo "FUNCIONARIO RESPONSÁVEL: " .  $cliente['funcResponsavel'] . " <br>";
+            echo "Endereco: " .  $cliente['endereco'] . " <br>";
+            echo "<br><br>";
+        }
+    }
+    ?>
+
+    <table border='1'>
+        <tr>
+            <td colspan='4'>
+                <center>
+                    Veiculos p/ devolução
+                </center>
+            </td>
+        </tr>
+        <tr>
+            <td>Veiculo</td>
+            <td>Data</td>
+            <td>Ação</td>
+        </tr>
+        <tr>
+            
+            <?php
 
             $veiculos = dadosAluguelIdcliente($conexao, $id_cliente);
 
@@ -66,9 +71,9 @@
                     $nome = $dados["nome"];
                     $id_veiculo = $dados["id"];
                     $id_aluguel = $dados["aluguel"];
-                   echo "<td>" . $dados["nome"] ."</td>";
-                   echo "<td>" . $dados["data"] . "</td>";
-                   echo "<td>
+                    echo "<td>" . $dados["nome"] . "</td>";
+                    echo "<td>" . $dados["data"] . "</td>";
+                    echo "<td>
                             <button>
                                 <a href='carrinho_devolucao.php?id_veiculo=$id_veiculo&nome_veiculo=$nome&cliente=$id_cliente&nome=$nome_cliente&id_aluguel=$id_aluguel&origem=2'> Devolução </a>
                             </button> 
@@ -76,36 +81,35 @@
                                 <a href='carrinho_devolucao.php?id_veiculo=$id_veiculo&nome_veiculo=$nome&cliente=$id_cliente&nome=$nome_cliente&id_aluguel=$id_aluguel&origem=1' style='color: white;'>selecionar</a>
                             </button>
                         </td>";
-                   
-                   echo "</tr>";
+
+                    echo "</tr>";
                 }
                 echo "</table>";
-            }
-            else {
+            } else {
                 echo "Não há veiculos alugados";
             }
-           
-        // Exibiçao do carrinho
-        echo '<form action="dados_devolucao.php">';
-        echo '<div class="position-absolute top-0 end-0" id="frame">';
-        if (empty($_SESSION['carrinho_devolucao']['veiculos_devolucao'])) {
-            echo "selecione alguns veiculos";
-        }else {    
-           $nome_veiculo = removerRepetidosArray($_SESSION['carrinho_devolucao']['veiculos_devolucao']);
-            foreach($nome_veiculo as $dados_veiculo) {
 
-                $nome = $dados_veiculo["nome"];
-                $id = $dados_veiculo["id_veiculo"];
+            // Exibiçao do carrinho
+            echo '<form action="dados_devolucao.php">';
+            echo '<div class="position-absolute top-0 end-0" id="frame">';
+            if (empty($_SESSION['carrinho_devolucao']['veiculos_devolucao'])) {
+                echo "selecione alguns veiculos";
+            } else {
+                $nome_veiculo = removerRepetidosArray($_SESSION['carrinho_devolucao']['veiculos_devolucao']);
+                foreach ($nome_veiculo as $dados_veiculo) {
 
-                echo"
+                    $nome = $dados_veiculo["nome"];
+                    $id = $dados_veiculo["id_veiculo"];
+
+                    echo "
                     <div class='card' style='width: 18rem;'>
                         <div class='card-body'>
                             <h5 class='card-title'> $nome </h5>
                         </div>
                     </div> <br>";
 
-                    
-                    echo '<input type="hidden" name="aluguel" value="' . $dados_veiculo["aluguel"] .'" >';
+
+                    echo '<input type="hidden" name="aluguel" value="' . $dados_veiculo["aluguel"] . '" >';
                 }
 
                 echo "<input type='hidden' name='nome' value='$nome_cliente' >";
@@ -116,11 +120,12 @@
 
                 echo "<input type='submit' value='Seguir &#10145;&#65039;'>";
                 echo '</div>';
-        }
-    ?>
-    </form>
+            }
+            ?>
+            </form>
 
- 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> 
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
